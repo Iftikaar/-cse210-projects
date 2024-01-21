3801b44c95613778d1691e2a2604db3745f46d8b
Iftikaar <iftikaar0905@gmail.com>
2024-01-21 09:49:46 +0200

    very poor attempt at journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,40 @@
+ 
+
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+
+
+ 
+
+
+
+
+
+
+ 
+
+
+ 
+
+    static void DisplayMenu()
+
     {
-        Console.WriteLine("Hello Develop02 World!");
+
+        Console.WriteLine("1. Add entry");
+
+        Console.WriteLine("2. Delete entry");
+
+        Console.WriteLine("3. Display entries");
+
+        Console.WriteLine("4. Save journal");
+
+        Console.WriteLine("5. Load journal");
+
+        Console.WriteLine("6. Exit");
+
     }
-}
+
+
+ // sir I was struggling with this programm for some reason. Is it possible to grade this programm I submitted and proved corrections.
+
+ // I also can not run my code as in the run section it does not allow me to select the exact cs code I want to run only the folders not the files. 
